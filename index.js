0cca9736a6234d2e61eaae7b05b05f74d1d6b06d
JaiShankar <jaishankar.a1603@gmail.com>
2022-06-27 16:37:04 +0530

    my first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+//TOGGLE EFFECT BUTTON
+var toggleButton = document.querySelector('.burger-lines');
+var navbarOption = document.querySelector('.nav-option-container');
+var toggleIcon1 = document.querySelector('.burger-lines .fa-xmark');
+var toggleIcon2 = document.querySelector('.burger-lines .fa-bars');
+
+toggleButton.onclick = function () {
+    if (navbarOption.style.display=="") {
+        navbarOption.style.display = "block";
+        toggleIcon1.style.display = "block";
+        toggleIcon2.style.display = "none";
+    }
+    else {
+        navbarOption.style.display = ""
+        toggleIcon1.style.display = "none";
+        toggleIcon2.style.display = "block";
+    }
+
+}
