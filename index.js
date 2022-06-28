a9f2a1d5278803d65d9ac2336af37b28afac1ee1
JaiShankar <jaishankar.a1603@gmail.com>
2022-06-28 12:29:09 +0530

    edited about section

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var toggleIcon1 = document.querySelector('.burger-lines .fa-xmark');
 var toggleIcon2 = document.querySelector('.burger-lines .fa-bars');
 
 toggleButton.onclick = function () {
-    if (navbarOption.style.display=="") {
+    if (navbarOption.style.display == "") {
         navbarOption.style.display = "block";
         toggleIcon1.style.display = "block";
         toggleIcon2.style.display = "none";
@@ -18,8 +18,8 @@ toggleButton.onclick = function () {
 
 }
 
-function hidenav(){
-    if(window.innerWidth <= 768){
+function hidenav() {
+    if (window.innerWidth <= 768) {
         navbarOption.style.display = "";
         toggleIcon1.style.display = "none";
         toggleIcon2.style.display = "block";
